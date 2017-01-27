b554e4141d8d4218847d982277f4a4448ef29c1e
Andrew Peters <anpete@microsoft.com>
2017-01-27 11:00:46 -0800

    WIP

--- src/Benchmarks/Data/EfDb.cs
+++ src/Benchmarks/Data/EfDb.cs
@@ -1,10 +1,13 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved. 
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Benchmarks.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.Extensions.Options;
 
 namespace Benchmarks.Data
@@ -19,14 +22,18 @@ namespace Benchmarks.Data
         {
             _random = random;
             _dbContext = dbContext;
-            _dbContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
             _useBatchUpdate = appSettings.Value.Database != DatabaseServer.PostgreSql;
         }
+
+        private static readonly Func<ApplicationDbContext, int, Task<World>> _firstWorldQuery
+            = EF.CompileAsyncQuery((ApplicationDbContext context, int id)
+                => context.World.First(w => w.Id == id));
 
         public Task<World> LoadSingleQueryRow()
         {
             var id = _random.Next(1, 10001);
-            return _dbContext.World.FirstAsync(w => w.Id == id);
+
+            return _firstWorldQuery(_dbContext, id);
         }
 
         public async Task<World[]> LoadMultipleQueriesRows(int count)
@@ -36,41 +43,61 @@ namespace Benchmarks.Data
             for (var i = 0; i < count; i++)
             {
                 var id = _random.Next(1, 10001);
-                result[i] = await _dbContext.World.FirstAsync(w => w.Id == id);
+
+                result[i] = await _firstWorldQuery(_dbContext, id);
             }
 
             return result;
         }
 
+        private static readonly Func<ApplicationDbContext, int, Task<World>> _firstWorldTrackedQuery
+            = EF.CompileAsyncQuery((ApplicationDbContext context, int id)
+                => context.World.AsTracking().First(w => w.Id == id));
+
         public async Task<World[]> LoadMultipleUpdatesRows(int count)
         {
-            var results = new World[count];
-
-            for (var i = 0; i < count; i++)
+            try
             {
-                var id = _random.Next(1, 10001);
-                var result = await _dbContext.World.AsTracking().FirstAsync(w => w.Id == id);
+                var results = new World[count];
 
-                result.RandomNumber = _random.Next(1, 10001);
-                results[i] = result;
+                for (var i = 0; i < count; i++)
+                {
+                    var id = _random.Next(1, 10001);
+                    var result = await _firstWorldTrackedQuery(_dbContext, id);
 
-                if (!_useBatchUpdate)
+                    _dbContext.Entry(result).Property("RandomNumber").CurrentValue = _random.Next(1, 10001);
+
+                    results[i] = result;
+
+                    if (!_useBatchUpdate)
+                    {
+                        await _dbContext.SaveChangesAsync();
+                    }
+                }
+
+                if (_useBatchUpdate)
                 {
                     await _dbContext.SaveChangesAsync();
                 }
-            }
-            
-            if (_useBatchUpdate)
-            {
-                await _dbContext.SaveChangesAsync();
-            }
 
-            return results;
+                Console.WriteLine("Success!");
+
+                return results;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+                throw;
+            }
         }
+
+        private static readonly Func<ApplicationDbContext, AsyncEnumerable<Fortune>> _fortunesQuery
+            = EF.CompileAsyncQuery((ApplicationDbContext context) => context.Fortune);
 
         public async Task<IEnumerable<Fortune>> LoadFortunesRows()
         {
-            var result = await _dbContext.Fortune.ToListAsync();
+            var result = await _fortunesQuery(_dbContext).ToListAsync();
 
             result.Add(new Fortune { Message = "Additional fortune added at request time." });
             result.Sort();
